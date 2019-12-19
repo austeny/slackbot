01e28a6b53962da6be312d8c375a5fe7e772d7ed
Austen Young <austen.d.young@gmail.com>
2019-12-18 20:50:56 -0500

    adding specific channel features

--- app3.js
+++ app3.js
@@ -21,7 +21,7 @@ console.log(ip_addr);
 const web = new WebClient(process.env.SLACK_TOKEN);
 //const slack_webhook = process.env.SLACK_WEBHOOK;
 
-function AptJob(name, time, text, channel_id){
+function AptJob(name, time, text, channel_id, channel_name){
      this.name = name;
      this.time = time;
      this.text = text;
@@ -50,17 +50,14 @@ app.post('/createjob', urlencodedParser, (req, res) => {
         console.log(args);
         let jobObj = new AptJob(args[0], args[1], args[2], req.body.channel_id, req.body.channel_name);
         let sJob = schedule.scheduleJob(args[0], args[1], () => {
-            // web.chat.postMessage({
-            //     channel: 'CJCHJU1JM',
-            //     text: args[2]
-            // })
-            console.log('scheduled job');
+            web.chat.postMessage({
+                channel: 'CJCHJU1JM',
+                text: args[2]
+            })
         })
         //add Job and AptJob objects to jobArray and jobObjArray 
         jobArray.push(sJob);
         jobObjArray.push(jobObj);
-        // console.log('jobArray: ', jobArray);
-        // console.log('jobObjArray: ', jobObjArray);
         res.json({text: `Created job`});
     }catch(e){
         console.log('error: ', e);
@@ -68,19 +65,15 @@ app.post('/createjob', urlencodedParser, (req, res) => {
     }
 })
 
-//createJob slash command
-//args(job name, CRON date, job response text)
+//createJob slash command - args(job name, CRON date, job response text)
 app.post('/canceljob', urlencodedParser, (req, res) => {
-    // try to create job with args passed in /createjob
+    console.log(req.body);
     try {
-        console.log(req.body.text);
         // get index of name of job
         let jobName = req.body.text;
         let index = jobArray.findIndex((item, i) => {
             return item.name === jobName
         });
-        console.log(index);
-
         //check if index was found, yes cancel and remove job from array, no reply bad search
         if(index >= 0){
             jobArray[index].cancel();
@@ -90,56 +83,29 @@ app.post('/canceljob', urlencodedParser, (req, res) => {
             res.json({text: `${jobName} was not found in list of current jobs`});
         }
     }catch(e){
+        console.log('error', e);
         res.json({text: `Hit error: ${e}`});
     }
 })
 
+//getjobs route, no arg needed
 app.post('/getjobs', urlencodedParser, (req, res) => {
     console.log(req.body);
-    // try to create job with args passed in /createjob
     try {
         let message = '';
-        console.log(jobObjArray.length);
         jobObjArray.forEach(e => {
-            message = `${message}${e.name} - ${e.time} - ${e.text}\n`;
+            if(e.channel_id == req.body.channel_id){
+                message = `${message}${e.name} - ${e.time} - ${e.text}\n`;
+            }
         })
         res.json({text: message});
     }catch(e){
+        console.log('error', e);
         res.json({text: `Hit error: ${e}`});
     }
 })
 
+// listening
 app.listen(process.env.NODE_PORT, ip_addr, () => {
     console.log(`Sever on port ${process.env.NODE_PORT}`);
 })
-
-//scheduled job will run at 8am on 28th of every month
-// let rentJob = schedule.scheduleJob('testJob1', '0 8 28 * *', function(){
-//     web.chat.postMessage({
-//         channel: 'CJCHJU1JM',
-//         text: "Make sure to pay rent"
-//     })
-// });
-// rentJob.cancel();
-
-// let fJob = schedule.scheduleJob('testjob1', '*/7 * * * * *', function(){
-//     console.log('fjob');
-// });
-// let sJob = schedule.scheduleJob('testjob2', '*/5 * * * * *', function(){
-//     console.log('sjob');
-// });
-// testArr.push(fJob);
-// testArr.push(sJob);
-// schedule.scheduleJob('*/10 * * * * *', async function(){
-//     try {
-//         const res = await request({
-//             url: `https://hooks.slack.com/services/${slack_webhook}`,
-//             method: 'POST',
-//             body: slackBody,
-//             json: true
-//         });
-//         console.log(res);
-//     } catch(e) {
-//         console.log(`caught error: ${e}`);
-//     }
-// });
